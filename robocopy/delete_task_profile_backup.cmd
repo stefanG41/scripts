50e7252e17fd18d26337aa770cbdc6acdc8cd079
stefanG41 <47351072+stefanG41@users.noreply.github.com>
2025-10-17 09:43:26 +0200

    Simplify delete task script for ProfileBackup

--- robocopy/delete_task_profile_backup.cmd
+++ robocopy/delete_task_profile_backup.cmd
@@ -1,5 +1,3 @@
 @echo off
-set "TASKNAME=ProfileBackup"
-echo [INFO] Versuche, den Task "%TASKNAME%" zu löschen...
-schtasks /delete /tn "%TASKNAME%" /f
+schtasks /delete /tn "ProfileBackup" /f
 pause
